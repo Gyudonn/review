180463ddfe65606814b2c2b87fc24a433943c0bc
Gyudonn <xjirmpmxjirmpm@gmail.com>
2021-07-01 22:57:47 +0900

    peview_py update

--- review.py
+++ review.py
@@ -10,15 +10,24 @@ print('There is ', len(data), ' data')
 
 wc = {} #word_count
 for d in data:
-	words = d.split(' ')
+	words = d.split()
 	for word in words:
 		if word in wc:
 			wc[word] += 1 
 		else:
 			wc[word] = 1
 
-for word in wc:
-		if wc[word] > 100:
-			print(word, wc[word])
+while True:
+	search = input('Please what word do you want to search: ')
+	if search == 'q':
+		break
+	if search in wc:
+		print(wc[word])
+	else:
+		print('Can not find this word: ', search)
 
-print(len(wc))
+# for word in wc:
+# 		if wc[word] > 100:
+# 			print(word, wc[word])
+
+# print(len(wc))
