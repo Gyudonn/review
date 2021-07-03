53f5305e1575d070e621bf5a55e810cacd40e916
Gyudonn <xjirmpmxjirmpm@gmail.com>
2021-07-03 23:03:15 +0900

    import_bar

--- review.py
+++ review.py
@@ -1,14 +1,22 @@
+import time
+import progressbar
+
 data = []
 count = 0
+bar = progressbar.ProgressBar(max_value=1000000)
 with open('reviews.txt', 'r') as file:
 	for line in file:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+		bar.update(count)
+		# if count % 1000 == 0:
+		# 	print(len(data))
+
 print('There is ', len(data), ' data')
 
-wc = {} #word_count
+#word_count
+start_time = time.time()
+wc = {} 
 for d in data:
 	words = d.split()
 	for word in words:
@@ -16,13 +24,16 @@ for d in data:
 			wc[word] += 1 
 		else:
 			wc[word] = 1
+end_time = time.time()
+print('Spends ', end_time - start_time, 'seconds to count word.')
 
 while True:
 	search = input('Please what word do you want to search: ')
 	if search == 'q':
+		print('Exit')
 		break
 	if search in wc:
-		print(wc[word])
+		print('Found ', search, ' in ', wc[word], ' times')
 	else:
 		print('Can not find this word: ', search)
 
